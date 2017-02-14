267ec31251b0250631f572df961479476720963f
datalal <lawsonw2@gmail.com>
2017-02-14 16:40:46 -0500

    seatBox fix

--- script.js
+++ script.js
@@ -2,46 +2,88 @@ $(document).ready(function() {
 
 function displaySeats() {
 
-for( i = 1; i < 7; i++){
+// for( i = 1; i < 4; i++){
+//
+// // move(append) into a row div with seats
+//
+//   var rowCount = document.createElement("div");
+//   rowCount.className = "rowCount";
+//   rowCount.textContent = "Row " + i;
+//   rowCount.id = "rowCount";
+//   $(".container").append(rowCount);
+//
+//     for (j = 1; j < 6; j++) {
+//
+//         // seatInfo.push({
+//         //     userName: "testUser",
+//         //     seatNumber: i,
+//         //     reserved: false
+//         // });
+//
+//       var seatBox = document.createElement("div");
+//         seatBox.className = "seatBox";
+//         seatBox.textContent = "Seat " + j;
+//         // seatBox.textContent = "seatBox" + " " + j + " " + seatInfo[i].seatNumber;
+//
+//         seatBox.id = "seatBox";
+//         $(".rowCount").append(seatBox);
+//         // seatInfo[0].reserved = true;
+//         // seatInfo[0].seatNumber = i;
+//
+//     };
+//
 
-// move(append) into a row div with seats
 
-  var rowCount = document.createElement("div");
-  rowCount.className = "rowCount";
-  rowCount.textContent = "Row " + i;
-  rowCount.id = "rowCount";
-  $(".container").append(rowCount);
+  // };
+  for( i = 1; i < 25; i++){
 
-    for (j = 1; j < 5; j++) {
-
-        // seatInfo.push({
-        //     userName: "testUser",
-        //     seatNumber: i,
-        //     reserved: false
-        // });
-
-        seatBox = document.createElement("div");
-        seatBox.className = "seatBox";
-        seatBox.textContent = "Seat " + j;
-        // seatBox.textContent = "seatBox" + " " + j + " " + seatInfo[i].seatNumber;
-
-        seatBox.id = "seatBox";
-        $(".container").append(seatBox);
-        // seatInfo[0].reserved = true;
-        // seatInfo[0].seatNumber = i;
-
-    };
+          var seatBox = document.createElement("div");
+            seatBox.className = "seatBox";
+            seatBox.textContent = "Seat " + i;
+            //seatBox.textContent = "seatBox" + " " + j + " " + seatInfo[i].seatNumber;
+            $(".container").append(seatBox);
 
 
-
-  };
+}
 
   var stage = document.createElement("div");
   stage.className = "stage";
   stage.textContent = "stage";
   $(".container").append(stage);
+
+
+  $(".seatBox").hover(
+      function() {
+          $(this).css("background-color", "red");
+
+      }).mouseout(function() {
+      $(this).css("background-color", "grey");
+  });
+
+
+  $(".seatBox").on("click",
+      function() {
+          $(this).css("background-color", "pink");
+          $(".formContainer").toggle();
+          var seatNumber = $(this).text().split(" ");
+          var rowNumber = $(this).parent.textContent;
+          console.log(rowNumber);
+           console.log("Seat Number:" + " " +seatNumber[1] +" " + "row: " + " " + rowNumber);
+      });
+
+
+
 };
 
+// $(".container.seatBox").mouseover(function(){
+//
+// $(this).css("opacity", "0.25");
+//
+// });
+//
+
+
+$(".formContainer").hide();
 
 
 displaySeats();
